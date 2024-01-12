9c04e37ecdac832e9a953250882b7ffabd94856c
theguyinpink <clement.carre.prevert.pro@gmail.com>
2024-01-12 22:56:14 +0100

    add projet

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://clement-carre-portfolio.netlify.app",
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 10000,
+    }),
+    robotsTxt(),
+  ],
+});
---
+++ src/data/info.ts
@@ -0,0 +1,129 @@
+
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Clément Carré BTS SLAM",
+  jobDescription: "Apprenti Développeur BTS SIO SLAM",
+  about: `je suis actuellement en BTS SIO option SLAM (Solutions Logicielles et Applications Métiers) au Lycée Jacques Prévert à Combs-la-Ville. 
+  Je suis passionné par l'informatique depuis mon plus jeune âge. 
+  J'ai toujours aimé apprendre de nouvelles choses et résoudre des problèmes.
+  `,
+
+  experience: [
+    {
+      name: "Groupe Avec",
+      location: "Vincennes-Paris, Ile-de-France, France",
+      startDate: "Nov 2021",
+      endDate: "Jun 2024",
+      description: [
+        "- Assistance aux utilisateurs",
+        "- Devellopement d'un ChatBot qui permet de determiner quelle maladie le patient possède",
+        "- Devellopement d'une page de connexion et déconnexion à un hotspot wifi dans nos agences",
+      ],
+    },
+
+    {
+      name: "Alturing",
+      location: "Paris, Ile-de-France, France",
+      startDate: "Jan 2021",
+      endDate: "Apr 2021",
+      description: [
+        "✓ Assistance aux collaborateurs",
+        "✓ Configuration poste de travail",
+        "✓ Installation de logiciel",
+        "✓ Ticketing",
+      ],
+    },
+
+    {
+      name: "Osmose Toilettage",
+      location: "Boussy-Saint-Antoine, Essone, France",
+      startDate: "Apr 2018",
+      endDate: "Apr 2018",
+      description: [
+        "- Un stage d'une semaine dans un toiletteur canin pour me former à la vie professionelle ",
+        "- Toilettage de chien principalement",
+        "- Apprendre à travailler en équipe"
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Collège Les AUlnres",
+      location: "Combs-la-Vile, Seine-et-Marne, France",
+      startDate: "2015",
+      endDate: "2019",
+      description: [
+        "Diplome National du Brevet des Collèges",
+      ],
+    },
+    {
+      name: "Lycée Professionel Jacques Prévert",
+      location: "Combs-la-Ville, Seine-et-Marne, France",
+      startDate: "2019",
+      endDate: "2022",
+      description: ["Diplome National du Baccalauréat, Mention Bien"],
+    },
+    {
+      name: "BTS SIO Lycée Professionel Jacques Prévert",
+      location: "Combs-la-Ville, Seine-et-Marne, France",
+      startDate: "2022",
+      endDate: "en cours",
+      description: ["Diplome Brevet Technicien Supérieur"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100088012343050",
+    twitter: "https://twitter.com/guy_in_pink",
+    github: "https://github.com/theguyinpink",
+    email: "mailto:clement.carre.prevert.pro@gmail.com",
+    linkedin: "https://www.linkedin.com/in/clément-carré-a6971624a/",
+  },
+
+
+  projects: [
+    {
+      title: "Festival",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Calculatrice",
+      isFeatured: true,
+      thumbnail: "/assets/images/calculatrice.png",
+      githubUrl: "https://github.com/theguyinpink/calculatrice",
+      liveUrl: "https://ccarre.malicebc.com/code%20calculatrice/",
+    },
+    {
+      title: "Music Player",
+      isFeatured: true,
+      thumbnail: "/assets/images/music-player.png",
+      githubUrl: "https://github.com/theguyinpink/music-player",
+      liveUrl: "https://ccarre.malicebc.com/music-player-master/",
+    },
+    {
+      title: "Site Météo",
+      isFeatured: true,
+      thumbnail: "/assets/images/meteo.png",
+      githubUrl: "https://github.com/theguyinpink/meteo",
+      liveUrl: "https://ccarre.malicebc.com/code%20app%20météo/",
+    },
+    {
+      title: "Flappy Bird",
+      isFeatured: true,
+      thumbnail: "/assets/images/flappy-bird.png",
+      githubUrl: "https://github.com/theguyinpink/flappy-bird",
+      liveUrl: "https://ccarre.malicebc.com/Flappy%20Bird/",
+    },
+    {
+      title: "Trading BTC View",
+      isFeatured: true,
+      thumbnail: "/assets/images/trading.png",
+      githubUrl: "https://github.com/theguyinpink/trading",
+      liveUrl: "https://ccarre.malicebc.com/Tradingview/",
+    },
+  ],
+};
